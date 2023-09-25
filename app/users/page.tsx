cb6f8386b78493435450c0ae6dab78ae818196b3
Gince George <gincegeo@gmail.com>
2023-09-25 14:43:30 +0530

    initial commit

---
+++ app/users/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      {new Date().toLocaleTimeString()}
+      <br />
+      <Link href="/">Home</Link>
+      <h1 className="font-bold text-2xl pb-5">Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
